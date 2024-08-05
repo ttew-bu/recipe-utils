a72ff6c1f4c2015ff057bba3d5ddbfac9db88fe8
ttew-bu <ttew@bu.edu>
2024-08-04 20:01:05 -0400

    clean up formatting and readme file

--- README.md
+++ README.md
@@ -1,13 +1,30 @@
-Many times, I've run into issues with cooking along with recipe websites. The recipes themselves can be awesome, but often these pages are littered with ads and prose that is sometimes interesting, but often gets in the way when I'm trying to gather an ingredient list and/or actually go through the steps to cook the dish.
-
-Since I assume that others run into similar issues, I wanted to create a tool that I'd use, but also create something that others could use to collect recipes and build their own personal repositories without needing to save links that may or may not persist over time.
-
 # RecipeSnipper Browser Extension Demo
-The following is a demo for a browser extension that pulls down recipes from the current site and returns a formatted template with the recipe metadata. 
+The following is a demo for a browser extension that pulls down recipes from the current site and returns a formatted JSON file with the recipe metadata. 
+
+Production backend is hosted on Heroku and powered by FastAPI
+
+As for why I've built this service, I've run into issues with accessibility and cooking websites. Often, these pages are filled with ads and are difficult to navigate; many require an email being added to a mailing list to pull a copy of the recipe. 
+
+Since I assume that others run into similar issues, I wanted to create a tool that I'd use to store recipes of interest in a way others may also benefit.
+
+The hope here is to allow folks to collect recipes and build their own personal repositories without needing to save links that may or may not persist over time or deal with the other accessibility challenges described above.
 
 ### Requirements
 - Python 3.12 and libraries contained within requirements.txt
-- OPENAI_API_TOKEN environment variable (can be set in .env file, bash_profile, or similar)
-- API_URL environment variable
-    - for local testing it should be this -> API_URL="http://127.0.0.1:8000/"
-    - there is no global server at this time, so unless you make your own, just use localhost
+- OPENAI_API_TOKEN environment variable (can be set in .env file, bash_profile, or similar), set this before spinning up API locally
+
+### Running the API Locally
+- The actual browser extension runs off of a hosted API for all requests, but if you're testing locally, do the following:
+sh run_server.sh
+
+### Running the Extension Locally
+- Open up Chrome, click on the puzzle piece for extension managemetn, and then click into manage extensions
+- Switch the slider for dev mode to on
+- Click "load unpacked"
+- In the file selector, click the extension folder (which should have the manifest, popup.html, and script.js)
+- You should now be able to trigger
+
+
+### Future Work
+- The intent is to publish the extension on the Chrome store and have that run against the back end in Heroku allowing for anyone to use this
+- The addition of user management and persistent storage of recipes are future considerations as well
--- extension/script.js
+++ extension/script.js
@@ -1,52 +1,39 @@
-// Trying to make analog to BeautifulSoup's getText here; minimize tokens going into API from misc scripring code
-const getText = () => {
-  return document.body.innerText;
-}
+// Defaults for the values that will change on the page
+var recipeResponse
+var siteText
+var currentURL
+var APIUrl = 'http://127.0.0.1:8000/'
+document.getElementById("current_status").innerText = 'Waiting for Text to Load'
 
 async function getCurrentTabUrl () {
-    console.log("Executing getCurrentTabUrl");
-    const tabs = await chrome.tabs.query({ active: true, currentWindow: true})
-    const current_url = tabs[0].url
-    document.getElementById("current_url").innerText =  current_url
-    return current_url
-  }
+  console.log("Executing getCurrentTabUrl");
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true})
+  currentURL = tab.url
+}
 
-async function getCurrentTabText(){
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  console.log("Executing getCurrentTabText");
-  chrome.scripting.executeScript({
-    target: { tabId: tabs[0].id },
-    func: getText
-  }, (result) => {
-    document.getElementById("current_status").innerText = 'Ready'
-
-    //persist the query result, return string
-    const site_text = result[0].result
-    console.log("getCurrentTabText length of ",site_text.length);
-    console.log("getCurrentTabText is ",site_text);
-    return site_text
+async function getCurrentTabText () {
+const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+let result;
+try {
+  [{result}] = await chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    func: () => document.documentElement.innerText,
   });
-});}
-
-//Always ready for the snip upon opening the tool
-
-getCurrentTabText();
-getCurrentTabUrl();
-
+} catch (e) {
+  document.body.textContent = 'Cannot access page';
+  return;
+}
+// process the result
+document.getElementById("current_status").innerText = 'Text Successfully Loaded'
+siteText = result;
+}
 
 async function  snipRecipe() {
-  // load current url based on active tab, also grab text
-  const current_url = await getCurrentTabUrl()
-  const site_text = await getCurrentTabText()
-  
-  console.log(current_url)
-  console.log('hey')
-  console.log(site_text)
 
   console.log(JSON.stringify({
-    url: current_url,
-    text: site_text
-  }))
+    url: currentURL,
+    text: siteText
+  }));
 
   const requestParams = {
     method: 'POST',
@@ -54,24 +41,51 @@ async function  snipRecipe() {
       'Content-type': 'application/json'
     },
     body: JSON.stringify({
-      url: current_url,
-      text: site_text
+      url: currentURL,
+      text: siteText
     })
   };
 
-  
+
   // Fetch from endpoint assigned
-  await fetch("http://127.0.0.1:8000/extract_from_text/", requestParams).then(async (response) => {
+  await fetch(APIUrl+"/extract_from_text/", requestParams).then(async (response) => {
     const data = await response.json();
     console.log(data)
-    document.getElementById("recipe_grabber_response_json").innerHTML = JSON.stringify(data)
-    document.getElementById("current_status").innerText = 'Grab Complete'
+
+    // create string representation of the object
+    recipeResponse = JSON.stringify(data, null, 2)
+
+    // offer differing text based on the response
+    if (data.has_recipe){
+    document.getElementById("current_status").innerText = 'Grab Complete, Recipe Found!'  
+    }
+
+    else {
+    document.getElementById("current_status").innerText = 'Grab Complete, No Recipe Found!'
+    }
+    console.log("Recipe response is: "+recipeResponse)
+
   });
+  }
+
+// Create hidden element and download object on click
+function downloadRecipe() {
+
+  var hiddenElement = document.createElement('a');
+  hiddenElement.href = 'data:text/plain;charset=utf-8,' + encodeURI(recipeResponse);
+  hiddenElement.target = '_blank';
+  hiddenElement.download = currentURL+'.json';
+  hiddenElement.click();
 }
 
 
-// Store default value
-// document.getElementById("recipe_grabber_response_json").defaultValue = "";
+// need to put these on listeners for page load, refresh, etc.
+
+getCurrentTabUrl()
+getCurrentTabText()
 
 // this function will generate the json object on click
 document.getElementById("snipButton").addEventListener("click", snipRecipe);
+
+// this function will generate the json object on click
+document.getElementById("downloadButton").addEventListener("click", downloadRecipe);
